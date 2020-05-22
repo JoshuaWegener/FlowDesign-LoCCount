e23bf902238cd505b27f1401282c89242e4e839c
Joshua Wegener <joshua.wegener@co-IT.eu>
2020-05-22 14:43:11 +0200

    datenobjekt hinzugefuegt

---
+++ src/LoCCount/Locstat.cs
@@ -0,0 +1,9 @@
+﻿namespace LoCCount
+{
+    internal class Locstat
+    {
+        public string Dateiname { get; set; }
+        public int AnzahlZeilen { get; set; }
+        public int AnzahlCodeZeilen { get; set; }
+    }
+}
