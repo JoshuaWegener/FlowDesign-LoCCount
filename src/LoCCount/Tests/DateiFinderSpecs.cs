6d11cd861ffeba3b22ed230c4b8f3815a77084ab
Joshua Wegener <joshua.wegener@co-IT.eu>
2020-05-22 15:44:06 +0200

    test samt framework hinzugefuegt

---
+++ src/LoCCount/Tests/DateiFinderSpecs.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+using FluentAssertions;
+using Xunit;
+
+namespace LoCCount.Tests
+{
+    public class DateiFinderSpecs
+    {
+        [Fact]
+        public void Anzahl_Dateien_In_Verzeichnis_Ist_8()
+        {
+            var startVerzeichnisPfad = @"D:\FlowDesign\LoCCount\";
+
+            var startVerzeichnis = new DirectoryInfo(startVerzeichnisPfad);
+            var dateiFinder = new DateiFinder(startVerzeichnis);
+
+            dateiFinder.AlleDateienImVerzeichnis().Should().BeEquivalentTo(8);
+        }
+    }
+}
