28b2e874e2296703158d31c64a176b39468e7bee
Joshua Wegener <joshua.wegener@co-IT.eu>
2020-05-22 15:08:14 +0200

    ausgabe hinzugefuegt

--- src/LoCCount/Program.cs
+++ src/LoCCount/Program.cs
@@ -1,4 +1,6 @@
-﻿using System.IO;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 
 namespace LoCCount
@@ -9,13 +11,53 @@ namespace LoCCount
         {
             var arg = @"D:\FlowDesign\LoCCount\";
 
-            var startVerzeichnis = new DirectoryInfo(arg);
+            var startVerzeichnis = LeseArgumente(arg);
 
+            var dateinamen = LeseDateinamen(startVerzeichnis);
+
+            var analyse = AnalysiereDateien(dateinamen);
+
+            KommandozeilenAusgabe(analyse);
+        }
+
+        private static void KommandozeilenAusgabe(IEnumerable<Locstat> analyse)
+        {
+            foreach (var locstat in analyse)
+            {
+                Console.WriteLine($"{locstat.Dateiname}: Zeilen:{locstat.AnzahlZeilen} | Code Zeilen:{locstat.AnzahlCodeZeilen}");
+            }
+
+            Console.WriteLine("Beliebige Taste drücken...");
+            Console.ReadKey();
+        }
+
+        private static IEnumerable<Locstat> AnalysiereDateien(IEnumerable<string> dateinamen)
+        {
+            var analyse = DateiAnalysierer.AlleDateien(dateinamen).ToList();
+
+            analyse.Add(new Locstat
+            {
+                Dateiname = "Gesamt",
+                AnzahlCodeZeilen = analyse.Sum(datei => datei.AnzahlCodeZeilen),
+                AnzahlZeilen = analyse.Sum(datei => datei.AnzahlZeilen)
+            });
+
+            return analyse;
+        }
+
+        private static IEnumerable<string> LeseDateinamen(DirectoryInfo startVerzeichnis)
+        {
             var dateiFinder = new DateiFinder(startVerzeichnis);
 
             var dateinamen = dateiFinder.AlleDateienImVerzeichnis();
+            return dateinamen;
+        }
 
-            var analyse = DateiAnalysierer.AlleDateien(dateinamen).ToList();
+        private static DirectoryInfo LeseArgumente(string arg)
+        {
+
+            var startVerzeichnis = new DirectoryInfo(arg);
+            return startVerzeichnis;
         }
     }
 }
